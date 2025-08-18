14a3be0dbad83dc991070e879e2632e70cac98b9
Marina Frankin <marinafrankin2005@gmail.com>
2025-08-18 08:15:46 -0300

    18/08/2025 - 08:12

---
+++ 18-08-2025/Array/Array_ArrayList_list/Program.cs
@@ -0,0 +1,14 @@
+﻿// Formas de criar array
+
+// Uma forma:
+string[] nomes = new string[5] { "Ana", "Maria", "Paula", "Marcos", "João" };
+
+// Segunda forma:
+int[] = new int[] { 1, 2, 3};
+
+string [] frutas = { "Maçã", "Banana", "Uva" };
+
+for(int x=0; x < 5; x++)
+{
+    Console.WriteLine(nomes[x]);
+}
